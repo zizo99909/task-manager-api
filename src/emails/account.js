a5b0a7f3a575505a0468eb0fe81b68ec56899929
Ziyad Wael <zizo99909@gmail.com>
2021-10-08 03:06:19 +0200

    Init commit

---
+++ src/emails/account.js
@@ -0,0 +1,28 @@
+const sgMail = require('@sendgrid/mail')
+
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const sendWelcomeEmail = (email,name)=>{
+    sgMail.send({
+        to:email,
+        from:'zizo99909@gmail.com',
+        subject:'Thanks for joining in!',
+        text:`Welcome to the app , ${name}. Let me know how you get along with the app.`
+    })
+}
+
+const sendCancelationEmail = (email,name)=>{
+    sgMail.send({
+        to:email,
+        from:'zizo99909@gmail.com',
+        subject:'The account had been cancelled',
+        text:`Hey ${name}, your account had been cancelled, can you contact us if there is any problem ?`
+    })
+
+}
+
+module.exports={
+    sendWelcomeEmail,
+    sendCancelationEmail
+}
